5094897731ea7cef0b18eedbae6e79901b24603c
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-03-18 16:17:40 +0100

    hotfix

--- src/components/fullComponent/Development.svelte
+++ src/components/fullComponent/Development.svelte
@@ -1,5 +1,5 @@
 <div
-	class="top-0 left-0 min-h-screen h-full flex flex-col items-center gap-16 py-48 px-8 md:py-56"
+	class="top-0 left-0 min-h-screen h-full flex flex-col items-center pt-48 px-8 md:pt-56"
 >
 	<h1
 		class="min-w-max h-full font-ms font-bold text-xl bg-gradient-to-r from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-2xl md:text-3xl lg:text-4xl"
