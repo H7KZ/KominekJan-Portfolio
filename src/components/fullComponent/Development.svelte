5c7f21eb3786afc5796df3bc567e1414e1816eaf
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-03-19 18:21:44 +0100

    add cute emote on development page

--- src/components/fullComponent/Development.svelte
+++ src/components/fullComponent/Development.svelte
@@ -1,15 +1,23 @@
-<div
-	class="top-0 left-0 min-h-screen h-full flex flex-col items-center pt-48 px-8 md:pt-56"
->
-	<h1
-		class="min-w-max h-full font-ms font-bold text-xl bg-gradient-to-r from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-2xl md:text-3xl lg:text-4xl"
-	>
-		<span class="text-grayWhite">Hello and</span><br /><span
-			class="text-5xl bg-gradient-to-tr from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-6xl md:text-7xl lg:text-8xl"
-			>Welcome</span
-		><br />
-		<span class="text-grayWhite">I am terribly sorry</span><br />
-		but this site <span class="text-grayWhite">is</span><br />
-		still in development<span class="text-grayWhite">.</span>
-	</h1>
+<div class="top-0 left-0 min-h-screen h-full flex justify-center pt-48 px-8 md:pt-56">
+	<div class="flex flex-col gap-6">
+		<h1
+			class="font-ms font-bold text-xl bg-gradient-to-r from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-2xl md:text-3xl lg:text-4xl"
+		>
+			<span class="text-grayWhite">Hello and</span><br /><span
+				class="text-5xl bg-gradient-to-tr from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-6xl md:text-7xl lg:text-8xl"
+				>Welcome</span
+			><br />
+			<span class="text-grayWhite">I am terribly sorry</span><br />
+			but this site <span class="text-grayWhite">is</span><br />
+			still in development<span class="text-grayWhite">.</span><br />
+		</h1>
+		<img src="cute.png" alt="cute" class="h-32" />
+		<h3
+			class="font-ms font-bold text-base bg-gradient-to-r from-[#EFFF3A] to-[#00ffc3d2] text-[#fff0] bg-clip-text sm:text-lg md:text-xl lg:text-2xl"
+		>
+			<span class="text-grayWhite">if you want to</span><br />
+			see the site in development<br />
+			<span class="italic text-grayWhite"><a href="https://dev.kominekjan.cz/">click here</a></span>
+		</h3>
+	</div>
 </div>
