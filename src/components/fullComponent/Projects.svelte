546d1b00c4ccb80e6bd5ab8725fb9062e6ed457d
H7KZ <74021016+H7KZ@users.noreply.github.com>
2022-06-07 12:34:08 +0200

    Optimization: webp instead of png

--- src/components/fullComponent/Projects.svelte
+++ src/components/fullComponent/Projects.svelte
@@ -8,7 +8,7 @@
 			title: 'Frontend for crypto site',
 			description:
 				'A site where I created landing page, login & singup page and dashboard. This entire job was done in framework Svelte and CSS framework TailwindCSS. Everything on the site is responsive and easily editable.',
-			image: '/projects/cryptolounge.png',
+			image: '/projects/cryptolounge.webp',
 			isLink: false,
 			link: ''
 		},
@@ -16,7 +16,7 @@
 			title: 'Frontend & backend of chat application',
 			description:
 				'A final school project for subject programming. This project was made with Typescript, MongoDB, ExpressJS, Svelte, TailwindCSS.',
-			image: '/projects/chatapp.png',
+			image: '/projects/chatapp.webp',
 			isLink: true,
 			link: 'https://chat.kominekjan.cz/',
 			linkName: 'chat.kominekjan.cz'
@@ -25,7 +25,7 @@
 			title: 'Admin role minecraft plugin',
 			description:
 				'A minecraft plugin that allows players to change from normal mode to admin mode. It saves the player inventory, armor, experiences etc. and then gives it back when player changes to the normal mode. It has an own configuration to specify which fields to save, to what gamemode to change. This plugin works with luckPerms and EssentialsX/EssentialsX Chat.',
-			image: '/projects/minecraft.png',
+			image: '/projects/minecraft.webp',
 			isLink: true,
 			link: 'https://github.com/H7KZ/AdminRole',
 			linkName: 'github.com/H7KZ/AdminRole'
